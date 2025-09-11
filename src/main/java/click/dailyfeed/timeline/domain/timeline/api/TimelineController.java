d82f30f4737ca5882e0e12828d02ace2a9b309e7
alpha3002025 <alpha300uk@gmail.com>
2025-09-11 10:28:17 +0900

    feat: (API) 내가 팔로잉한 멤버들의 최근 소식

---
+++ src/main/java/click/dailyfeed/timeline/domain/timeline/api/TimelineController.java
@@ -0,0 +1,33 @@
+package click.dailyfeed.timeline.domain.timeline.api;
+
+import click.dailyfeed.code.domain.timeline.timeline.dto.TimelineDto;
+import click.dailyfeed.code.global.web.response.DailyfeedScrollResponse;
+import click.dailyfeed.timeline.domain.timeline.service.TimelineService;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/timeline")
+public class TimelineController {
+    private final TimelineService timelineService;
+
+    @GetMapping("/followings")
+    public DailyfeedScrollResponse<TimelineDto.TimelinePostActivity> getMyFollowingMembersTimeline(
+            @RequestHeader("Authorization") String token,
+            HttpServletResponse response,
+            @PageableDefault(size = 20, sort = "updatedAt") Pageable pageable
+    ){
+        return timelineService.getMyFollowingMembersTimeline(pageable, token, response);
+    }
+
+
+    // 추천피드 이런거 하고 싶은데, 지금 시간이 많지 않다. 프론트엔드 까지 개발 완료 후에 검토해보기
+
+}
