cbc8a696e1bc8aa9cb64f1ac71efac93cab057cb
alpha3002025 <alpha300uk@gmail.com>
2025-11-05 14:01:37 +0900

    chore: prometheus

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,6 +55,10 @@ allprojects {
         implementation("org.springframework.boot:spring-boot-starter-data-redis")
         implementation("org.springframework.boot:spring-boot-starter-validation")
         implementation("org.springframework.boot:spring-boot-starter-web")
+        implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+        // micrometer & prometheus
+        runtimeOnly("io.micrometer:micrometer-registry-prometheus")
 
         // kafka
         implementation("org.springframework.kafka:spring-kafka")
